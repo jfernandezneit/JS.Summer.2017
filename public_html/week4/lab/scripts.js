ae2beae58c90083787e694ceb8a0238875814657
005011648 <005011648@N101A1.labs.neit.edu>
2017-08-07 20:48:21 -0400

    Merge origin/master
    Conflicts:
    	.gitignore
    	public_html/week3/demo/events-form-focus.html
    	public_html/week3/demo/events.html

--- public_html/week4/lab/scripts.js
+++ public_html/week4/lab/scripts.js
@@ -1,32 +1,76 @@
-let input =  window.document.querySelector('#num')
+let input = window.document.querySelector('#num')
 let button = window.document.querySelector('#btnGenerate')
 let div = window.document.querySelector('#wrapper')
 
 
+
+
 button.addEventListener('click', generateArray)
 
-function generateArray () {
-    div.innerHTML = input.value
+function generateArray() {
+    let initiator = parseInt(input.value, 10);
+
+    var items = []
+    var str = ' '
+    var total = 0;
+    var cntr = 0
+    var average = 0
+
+    str += '<table border="1">';
+
+    for (let row = 0; row < initiator; row++) {
+        items[row] = [];
+        str += '<tr>'
+
+        for (let column = 0; column < initiator; column++) {
+            items[row][column] = randomNumber(1, 100);
+            let value = items[row][column]
+            total += value
+
+            str += '<td>' + items[row][column] + '</td>';
+
+            if (value % 3 === 0) {
+
+                str += '<td class = "red">' + items[row][column] + '</td>';
+
+            } else if (value % 2 === 0) {
+                str += '<td class = "blue">' + items[row][column] + '</td>';
+            } else {
+                str += '<td>' + items[row][column] + '</td>';
+            }
+            cntr++
+
+
+        }
+
+        str += '</tr>'
+
+    }
+    str += '</table>';
+    average = (total / cntr)
+    console.log(average)
+    str += 'Average is: ' + average
+
+    div.innerHTML = str
+
+}
+function randomNumber(min, max) {
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+
 }
 
 
 
 
-//parseInt(input,10);
 
-/*if (input / 3 ===0)
-{
-    background.color = 'red'
-    
-}
-else if (input / 2 === 0)
-{
-    background.color = 'blue'
-}*/
+
+
+
 
 /*function randomNumber (min, max) {
-
-           return Math.floor (Math.random() * (max - min + 1)) + min;
-
-}*/
+ 
+ return Math.floor (Math.random() * (max - min + 1)) + min;
+ 
+ }*/
 
